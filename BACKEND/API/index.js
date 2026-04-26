544a9ea51ff7134186c3fe3fa56636e6a7eee6ff
Gabriel Passos Santana Dos Santos <gabrielpassos.sds@gmail.com>
2026-04-26 14:35:45 -0300

    Corrigindo pastas e subindo arquivos reais

---
+++ BACKEND/API/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const routes = require('./routes/routes');
+app.use('/api', routes);
+
+const mongoURL = process.env.MONGO_URI; 
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(mongoURL)
+  .then(() => {
+      console.log("Database Connected");
+  })
+  .catch((err) => {
+      console.log("MongoDB connection error:", err);
+  });
+
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Server Started at ${PORT}`);
+});
