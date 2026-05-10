d7609ac0b3cbe33638c3c3ee3fccc5498e17be27
Gabriel Passos Santana Dos Santos <gabrielpassos.sds@gmail.com>
2026-05-10 13:11:18 -0300

    Refactor HTTP requests to include headers

--- FRONTEND/TODOapp/src/app/app.component.ts
+++ FRONTEND/TODOapp/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Tarefa } from "./tarefa";
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -20,44 +19,48 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {
     this.apiURL = 'https://apitarefasgabriel255751.onrender.com';
-    this.READ_tarefas();
+    // Removido o READ_tarefas daqui para não tentar carregar antes do login
   }
 
   CREATE_tarefa(descricaoNovaTarefa: string) {
- var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
- this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
- resultado => { console.log(resultado); this.READ_tarefas(); });
-}
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    const idToken = new HttpHeaders().set("id-token", JSON.parse(this.tokenJWT).token);
+    this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa, { 'headers': idToken }).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
 
-READ_tarefas() {
-const idToken = new HttpHeaders().set("id-token", JSON.parse(this.tokenJWT).token);
-this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`, { 'headers': idToken }).subscribe(
-(resultado) => { this.arrayDeTarefas.set(resultado); this.usuarioLogado.set(true) },
-(error) => { this.usuarioLogado.set(false) }
-)
-}
+  READ_tarefas() {
+    const idToken = new HttpHeaders().set("id-token", JSON.parse(this.tokenJWT).token);
+    this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`, { 'headers': idToken }).subscribe(
+      (resultado) => { 
+        this.arrayDeTarefas.set(resultado); 
+        this.usuarioLogado.set(true); 
+      },
+      (error) => { 
+        this.usuarioLogado.set(false); 
+      }
+    )
+  }
 
- DELETE_tarefa(tarefaAserRemovida: Tarefa) {
- var indice = this.arrayDeTarefas().indexOf(tarefaAserRemovida);
- var id = this.arrayDeTarefas()[indice]._id;
- this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
- resultado => { console.log(resultado); this.READ_tarefas(); });
- }
+  DELETE_tarefa(tarefaAserRemovida: Tarefa) {
+    var id = tarefaAserRemovida._id;
+    const idToken = new HttpHeaders().set("id-token", JSON.parse(this.tokenJWT).token);
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`, { 'headers': idToken }).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
 
- UPDATE_tarefa(tarefaAserModificada: Tarefa) {
- var indice = this.arrayDeTarefas().indexOf(tarefaAserModificada);
- var id = this.arrayDeTarefas()[indice]._id;
- this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
- tarefaAserModificada).subscribe(
- resultado => { console.log(resultado); this.READ_tarefas(); });
- }
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    var id = tarefaAserModificada._id;
+    const idToken = new HttpHeaders().set("id-token", JSON.parse(this.tokenJWT).token);
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`, tarefaAserModificada, { 'headers': idToken }).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
 
- login(username: string, password: string) {
-var credenciais = { "nome": username, "senha": password }
-this.http.post(`${this.apiURL}/api/login`, credenciais).subscribe(resultado => {
-this.tokenJWT = JSON.stringify(resultado);
-this.READ_tarefas();
-});
-}
-
+  login(username: string, password: string) {
+    var credenciais = { "nome": username, "senha": password }
+    this.http.post(`${this.apiURL}/api/login`, credenciais).subscribe(resultado => {
+      this.tokenJWT = JSON.stringify(resultado);
+      this.READ_tarefas();
+    });
+  }
 }
